cf41ad8b28f26ed359dacec4f7eccf167b908fa7
Максим <maksimastafev540@gmail.com>
2025-01-23 14:34:56 +0500

    Update README.js

--- README.js
+++ README.js
@@ -1,11 +1,13 @@
-def calc(a, b):
-    if a == 0 or b == 0:
-        return "Операция невозможна"
-    elif a % 2 == 0 and b % 2 == 0:
-        return a + b
-    else:
-        return a - b
+function calc(a, b) {
+    if (a === 0 || b === 0) {
+        return "Операция невозможна";
+    } else if (a % 2 === 0 && b % 2 === 0) {
+        return a + b;
+    } else {
+        return a - b;
+    }
+}
 
-print(calc(4, 6))  
-print(calc(3, 5))  
-print(calc(0, 5))  
+console.log(calc(4, 6));  
+console.log(calc(3, 5));  
+console.log(calc(0, 5));   
